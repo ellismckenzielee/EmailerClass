f98f68ef69434aab40911928d07b49e4c7edc42e
Ellis Lee <ellismckenzielee@gmail.com>
2020-10-27 00:18:22 +0000

    completed ProgressEmailer for sending progress

--- emailer.py
+++ emailer.py
@@ -1,22 +1,20 @@
 import smtplib, ssl
 from email.message import EmailMessage
 
-#Will eventually be able to be used as a decorator
+#Simple class that will notify me upon completion of a long script.
 class ProgressEmail():
-    def __init__(self, sender, recipient, subject, password):
-        '''Init method requires sender and recipient email address, the email subject
-        and the password'''
-        self.sender = sender
+    def __init__(self, sender, recipient, password):
         self.recipient = recipient
-        self.subject = subject
+        self.sender = sender
         self.password = password
 
-    def sendEmail(self, content):
+
+    def sendEmail(self, subject, content):
         '''sendEmail creates and sends an email to the recipient, using content provided
-        as an argument'''
+        as an argument'''   
         port = 465  
         message = EmailMessage()
-        message['Subject'] = self.subject
+        message['Subject'] = subject
         message['From'] = self.sender
         message['To'] = self.recipient
         message.set_content(content)
@@ -24,11 +22,3 @@ class ProgressEmail():
         server.login(self.sender, self.password)
         server.send_message(message, self.recipient)
         server.close()
-
-if __name__=='__main__':
-    sender = input('Please enter the sender: ')
-    password = input('Please enter password: ')
-    recipient = input('Please enter recipient: ')
-    subject = input('Please enter subject')
-    email = ProgressEmail(sender, recipient, subject, password)
-    email.sendEmail('This is the email content.')
